72691194d243717fcb46c7fde6690c96080b505b
believenuhman <hylandjervon@gmail.com>
2026-05-23 16:29:54 +0000

    Fix Expo web blank screen

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,7 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-const defaultBlockList = config.resolver.blockList
-  ? [].concat(config.resolver.blockList)
-  : [];
-
-config.resolver.blockList = [
-  ...defaultBlockList,
-  /\.local\/skills\/.*/,
-  /\.local\/state\/.*/,
-  /\.local\/tasks\/.*/,
-  new RegExp(`^${__dirname.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}/web/.*`),
-];
+config.resolver.unstable_enablePackageExports = false;
 
 module.exports = config;
