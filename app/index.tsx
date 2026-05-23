6fd2349ad1ceb535607d8de8aac7e48787194fe7
believenuhman <hylandjervon@gmail.com>
2026-05-23 19:05:33 +0000

    Add root test page

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "black", alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ color: "white", fontSize: 30 }}>SITE WORKS</Text>
+    </View>
+  );
+}
