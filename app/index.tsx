28e8df06614e36c9986ff5addbf4cd39f1b81520
believenuhman <hylandjervon@gmail.com>
2026-05-23 19:12:59 +0000

    Restore real app after test

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View style={{ flex: 1, backgroundColor: "black", alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ color: "white", fontSize: 30 }}>SITE WORKS</Text>
-    </View>
-  );
-}
