cd281f76307ce2d2272bd6d301c981ba1cb137dd
believenuhman <hylandjervon@gmail.com>
2026-05-23 19:45:08 +0000

    Fix corrupted pasted files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,9 @@ import { Text, View } from "react-native";
 export default function Home() {
   return (
     <View style={{ flex: 1, backgroundColor: "black", alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ color: "white", fontSize: 28 }}>HD EXQUISITE HOME WORKS</Text>
+      <Text style={{ color: "white", fontSize: 28 }}>
+        HD EXQUISITE HOME WORKS
+      </Text>
     </View>
   );
 }
